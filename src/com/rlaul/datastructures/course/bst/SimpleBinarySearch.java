672ff8eaba5b398c46a40d39bb25b8ed780a7cdf
Ro-the-pro <rohit.laul@gmail.com>
2020-08-07 09:13:12 -0400

    Added number of retries in BST

--- src/com/rlaul/datastructures/course/bst/SimpleBinarySearch.java
+++ src/com/rlaul/datastructures/course/bst/SimpleBinarySearch.java
@@ -1,26 +1,31 @@
 package com.rlaul.datastructures.course.bst;
 
+import java.util.ArrayList;
+
 //Java implementation of recursive Binary Search 
 //https://www.geeksforgeeks.org/binary-search/
 
 class SimpleBinarySearch { 
+	int numberOfTries = 0;
+ 
  // Returns index of x if it is present in arr[l..r], else return -1 
- int binarySearch(int arr[], int left, int right, int x) 
- { 
+ int binarySearch(Integer[] array, int left, int right, int x) 
+ { 	 
+	 numberOfTries++;
      if (right >= left) { 
          int mid = left + (right - left) / 2; 
 
          // If the element is present at the middle itself 
-         if (arr[mid] == x) 
+         if (array[mid] == x) 
              return mid; 
 
          // If element is smaller than mid, then 
          // it can only be present in left subarray 
-         if (arr[mid] > x) 
-             return binarySearch(arr, left, mid - 1, x); 
+         if (array[mid] > x) 
+             return binarySearch(array, left, mid - 1, x); 
 
          // Else the element can only be present in right subarray 
-         return binarySearch(arr, mid + 1, right, x); 
+         return binarySearch(array, mid + 1, right, x); 
      } 
 
      // We reach here when element is not present in array 
@@ -31,13 +36,20 @@ class SimpleBinarySearch {
  public static void main(String args[]) 
  { 
 	 SimpleBinarySearch ob = new SimpleBinarySearch(); 
-     int arr[] = { 2, 3, 4, 10, 40 }; 
-     int n = arr.length; 
+	 ArrayList<Integer> list = new ArrayList<Integer>();
+	 int size = 100;
+	 
+     for (int i=0;i<size;i++) {
+    	 list.add(i);
+     }
+     Integer[] array = new Integer[list.size()];
+     array = list.toArray(array);
+     
      int searchNumber = 10; 
-     int result = ob.binarySearch(arr, 0, n - 1, searchNumber); 
+     int result = ob.binarySearch(array, 0, size - 1, searchNumber); 
      if (result == -1) 
          System.out.println("Element not present"); 
      else
-         System.out.println("Element found at index " + result); 
+         System.out.println("Element found at index " + result + " in number of tries="+ob.numberOfTries); 
  } 
 } 
