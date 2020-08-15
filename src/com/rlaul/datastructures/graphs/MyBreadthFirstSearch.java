312b4c91cabab63e0f3ade02562a3a67510a769d
Ro-the-pro <rohit.laul@gmail.com>
2020-08-15 15:56:55 -0400

    variables renamed to make sense

--- src/com/rlaul/datastructures/graphs/MyBreadthFirstSearch.java
+++ src/com/rlaul/datastructures/graphs/MyBreadthFirstSearch.java
@@ -10,55 +10,55 @@ import java.util.LinkedList;
 	// representation
 	public class MyBreadthFirstSearch
 	{
-	    private int V;   // No. of vertices
-	    private LinkedList<Integer> adj[]; //Adjacency Lists
+	    private int verticesCount;   // No. of vertices
+	    private LinkedList<Integer> adjacentNodes[]; // Adjacency Lists
 	 
 	    // Constructor
 	    MyBreadthFirstSearch(int v)
 	    {
-	        V = v;
-	        adj = new LinkedList[v];
+	        verticesCount = v;
+	        adjacentNodes = new LinkedList[v];
 	        for (int i=0; i<v; ++i)
-	            adj[i] = new LinkedList();
+	            adjacentNodes[i] = new LinkedList();
 	    }
 	 
 	    // Function to add an edge into the graph
 	    void addEdge(int v,int w)
 	    {
-	        adj[v].add(w);
+	        adjacentNodes[v].add(w);
 	    }
 	 
 	    // prints BFS traversal from a given source s
-	    void BFS(int s)
+	    void BFS(int currentNode)
 	    {
 	        // Mark all the vertices as not visited(By default
 	        // set as false)
-	        boolean visited[] = new boolean[V];
+	        boolean visited[] = new boolean[verticesCount];
 	 
 	        // Create a queue for BFS
 	        LinkedList<Integer> queue = new LinkedList<Integer>();
 	 
 	        // Mark the current node as visited and enqueue it
-	        visited[s]=true;
-	        queue.add(s);
+	        visited[currentNode]=true;
+	        queue.add(currentNode);
 	 
 	        while (queue.size() != 0)
 	        {
 	            // Dequeue a vertex from queue and print it
-	            s = queue.poll();
-	            System.out.print(s+" ");
+	            currentNode = queue.poll();
+	            System.out.print(currentNode+" ");
 	 
 	            // Get all adjacent vertices of the dequeued vertex s
 	            // If a adjacent has not been visited, then mark it
 	            // visited and enqueue it
-	            Iterator<Integer> i = adj[s].listIterator();
+	            Iterator<Integer> i = adjacentNodes[currentNode].listIterator();
 	            while (i.hasNext())
 	            {
-	                int n = i.next();
-	                if (!visited[n])
+	                int adjacentNode = i.next();
+	                if (!visited[adjacentNode])
 	                {
-	                    visited[n] = true;
-	                    queue.add(n);
+	                    visited[adjacentNode] = true;
+	                    queue.add(adjacentNode);
 	                }
 	            }
 	        }
