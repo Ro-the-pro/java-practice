9fa33e327e3e1fef364dc50915fab892a77e1053
Ro-the-pro <rohit.laul@gmail.com>
2022-01-12 22:51:36 -0500

    Merge two sorted lists

---
+++ src/com/rlaul/leetcode/easy/MergeTwoLists.java
@@ -0,0 +1,45 @@
+package com.rlaul.leetcode.easy;
+/**
+ * Definition for singly-linked list.
+ */
+ class ListNode {
+	 int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+public class MergeTwoLists {
+	
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		if(l1 ==null)
+			return l2;
+		if(l2 == null)
+			return l1; 
+		
+		if (l1.val < l2.val) {
+			l1.next = mergeTwoLists(l1.next, l2);
+			return l1;
+		}
+		else {
+			l2.next = mergeTwoLists(l1, l2.next);
+			return l2; 
+		}
+	}
+
+	public static void main(String[] args) {
+		ListNode l14 = new ListNode(4);
+		ListNode l12 = new ListNode(2, l14);
+		ListNode l11 = new ListNode(1,l12);
+		
+		ListNode l23 = new ListNode(3);
+		ListNode l22 = new ListNode(2, l23);
+		ListNode l21 = new ListNode(1,l22);
+		
+		MergeTwoLists obj = new MergeTwoLists();
+		ListNode resultListNode = obj.mergeTwoLists(l11, l21);
+		System.out.println(resultListNode);
+	}
+
+}
