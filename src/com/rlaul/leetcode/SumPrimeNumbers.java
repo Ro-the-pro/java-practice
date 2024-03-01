0b581debb366dba0a32ef658f6550b38b089f45f
Ro-the-pro <rohit.laul@gmail.com>
2024-02-29 20:50:47 -0500

    sum of prime numbers upto n

---
+++ src/com/rlaul/leetcode/SumPrimeNumbers.java
@@ -0,0 +1,38 @@
+package com.rlaul.leetcode;
+
+/**
+ * Given a number ‘n’, write a program to get the sum of all prime numbers from 0 to n.
+ * @author rohit
+ *
+ */
+public class SumPrimeNumbers {
+
+	public static void main(String[] args) {
+		int sum = findSumPrime(7);
+		System.out.println("sum="+sum);
+	}
+	
+	private static int findSumPrime (int num) {
+		int sum = 1;
+		for(int i=1;i<=num;i++) {
+			if(isPrime(i)) {
+				sum+=i;
+			}
+		}
+		return sum;
+	}
+
+	private static boolean isPrime(int number) {
+		int i = 2; 
+		while(i<=number/2) {
+			if(number%i==0) {
+				return false;
+			}
+			++i;
+		}
+		return true;
+	}
+	
+	
+
+}
