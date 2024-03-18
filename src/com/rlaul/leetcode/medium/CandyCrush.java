4b2fec144445fe91af158d8d88682557c50bda4c
Ro-the-pro <rohit.laul@gmail.com>
2024-03-18 12:30:06 -0400

    candy crush

--- src/com/rlaul/leetcode/medium/CandyCrush.java
+++ src/com/rlaul/leetcode/medium/CandyCrush.java
@@ -1,11 +1,19 @@
 package com.rlaul.leetcode.medium;
 
-
+/*
+ * { 
+ * {1, 1, 1, 5}, 
+ * {2, 2, 3, },
+ * {3, 3, 2}
+ * };
+ * 
+ * 
+ */
 //Step 1:  Crush Rows
 
 //Step 2: Crush Columns 
 
-//Step 3: Gravity
+//Step 3: Gravity - Remove 0's and move the cells down
 
 //Step 4: Repeat 1,2,3
 
@@ -16,15 +24,30 @@ public class CandyCrush {
 	public static void main(String[] args) {
 		int[][] board = { {1, 1, 1}, {2, 2, 3},{3, 3, 2}};
 		
-		int[][] result = crush(board);
+		crush(board);
+		print(board);
 	}
 	
 	
+	private static void print(int[][] board) {
+		System.out.println("{");
+		for(int r=0;r<board.length;r++) {
+			System.out.print("{");
+			for(int c=0;c<board[0].length;c++) {
+				System.out.print(board[r][c]+",");
+			}
+			System.out.print("}\n");
+		}
+		System.out.print("}");
+		
+	}
+
+
 	//crush
-	static int[][] crush(int[][] board){
+	static boolean crush(int[][] board){
 		boolean hasCandyCrush = false;
-		for (int c=0; c< board[0].length; c++) {
-			for (int r = 0; r < board.length; r++) {
+		for (int c=0; c< board[0].length-2; c++) {
+			for (int r = 0; r < board.length-2; r++) {
 				if(Math.abs(board[r][c])!=0 && Math.abs(board[r][c]) == Math.abs(board[r][c+1])
 						&& Math.abs(board[r][c]) == Math.abs(board[r][c+2])) {
 					//rows
@@ -52,13 +75,28 @@ public class CandyCrush {
 					}
 				}
 			}
+		return hasCandyCrush;
 		
-		return board;
 	}
 	
-	static int[][] gravitate(int[][] board){
+	//WIP
+	static void gravitate(int[][] board){
 		
-		return board;
+		for (int c=0; c< board[0].length; c++) {
+			for (int r = 0; r < board.length-2; r++) {
+				if(board[r][c]==0 && board[r+1][c]==0 && board[r+2][c]==0) {
+					if(r==0) {
+						board[r][c] = -1;
+					} else {
+						int[] temp = {0,0,0};
+						//board
+						
+					}
+					
+				}
+			}
+		}
+		
 	}
 
 }
