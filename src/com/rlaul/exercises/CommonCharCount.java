115821f251c0c713eb904a24639caadbfc916895
Ro-the-pro <rohit.laul@gmail.com>
2024-03-18 19:46:21 -0400

    Lucky number

--- src/com/rlaul/exercises/CommonCharCount.java
+++ src/com/rlaul/exercises/CommonCharCount.java
@@ -1,10 +1,7 @@
 package com.rlaul.exercises;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Set;
 
 public class CommonCharCount {
---
+++ src/com/rlaul/exercises/LuckyNumber.java
@@ -0,0 +1,37 @@
+package com.rlaul.exercises;
+
+/**
+ * Ticket numbers usually consist of an even number of digits. A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
+
+Given a ticket number n, determine if it's lucky or not.
+ * @author rohit
+ *
+ */
+public class LuckyNumber {
+
+	public static void main(String[] args) {
+		System.out.println(solution(239017));
+	}
+	
+	static boolean solution(int n) {
+	    Integer num = n;
+	    String numStr = num.toString();
+	    int numLength = numStr.length();
+	    int leftSum = 0;
+	    int rightSum = 0;
+	    
+	    if(numLength %2 !=0 )
+	        return false;
+	        
+	    for (int i=0, j=numLength-1;i<numLength/2;i++, j--){
+	        int leftHalfNum = numStr.charAt(i);
+	        leftSum+=leftHalfNum;
+	        int rightHalfNum = numStr.charAt(j);
+	        rightSum+=rightHalfNum;
+	    }
+	    
+	    return leftSum==rightSum;
+	}
+
+
+}
